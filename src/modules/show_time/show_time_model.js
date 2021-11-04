2264672be8a7ad5ea23d5b45f5d16b33be4d95e2
elazsyahranie <elazaribrahims@gmail.com>
2021-11-04 21:58:03 +0700

    Added show time

--- src/modules/show_time/show_time_model.js
+++ src/modules/show_time/show_time_model.js
@@ -1,110 +1,12 @@
 const connection = require('../../config/mysql')
 
 module.exports = {
-  getDataById: (id) => {
+  getAllShowTime: () => {
     return new Promise((resolve, reject) => {
-      connection.query(
-        `SELECT * FROM premiere JOIN movie ON premiere.movie_id = movie.movie_id WHERE premiere.premiere_id = ${id}`,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-  getDataByPremiereId: (premiereId) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        `SELECT * FROM show_time WHERE premiere_id = ${premiereId}`,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-  getMovieDataById: (movieId) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'SELECT * FROM movie WHERE movie_id = ?',
-        movieId,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-  getMovieDataByItsName: (movieName) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'SELECT * FROM movie WHERE movie_name = ?',
-        movieName,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-  getPremiereByItsName: (premiereName) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'SELECT * FROM premiere WHERE premiere_name = ?',
-        premiereName,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-  getLocationByItsId: (location) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'SELECT * FROM location WHERE location_id = ?',
-        location,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-  updateData: (setData, id) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        `UPDATE premiere SET ? WHERE premiere_id = ${id}`,
-        setData,
-        (error, result) => {
-          if (!error) {
-            const newResult = {
-              id: id,
-              ...setData
-            }
-            resolve(newResult)
-          } else {
-            reject(new Error(error))
-          }
-        }
-      )
-    })
-  },
-  getDataByCondition: (condition) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'SELECT * FROM user WHERE ?',
-        condition,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
-    })
-  },
-
-  deleteData: (id) => {
-    return new Promise((resolve, reject) => {
-      connection.query(
-        'DELETE FROM user WHERE user_id = ?',
-        id,
-        (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
-        }
-      )
+      connection.query('SELECT * FROM show_time', (error, result) => {
+        console.log(error)
+        !error ? resolve(result) : reject(new Error(error))
+      })
     })
   }
 }
--- src/modules/show_time/show_time_routes.js
+++ src/modules/show_time/show_time_routes.js
@@ -1,21 +1,7 @@
 const express = require('express')
 const Route = express.Router()
-// const authController = require('./auth_controller')
+const { allShowTime } = require('./show_time_controller')
 
-const {
-  newpremiere,
-  getMovieDataId,
-  getShowTimeByPremiereId
-} = require('./show_time_controller')
-
-Route.post('/', newpremiere)
-// Route.get('/:movieId/:locationId', getpremiereMovieLocation)
-Route.get('/:premiereId', getShowTimeByPremiereId)
-Route.get('/premiere_movie/:id', getMovieDataId)
-// Route.patch('/:id', updatePremiere)
-// Route.delete('/:id', deletePremiere)
-
-// Route.get('/change-data/:token', authController.changeData)
-// Route.post('/request-change-password', authController.requestChangePassword)
+Route.get('/', allShowTime)
 
 module.exports = Route
